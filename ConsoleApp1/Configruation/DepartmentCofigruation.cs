5e432a2e3610d58665cc54620bd32f304499e3b5
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-02-22 00:57:51 +0200

    Loading Navigational

---
+++ ConsoleApp1/Configruation/DepartmentCofigruation.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Configruation
+{
+    internal class DepartmentCofigruation : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(100, 10);
+            builder.Property(D => D.Name).HasColumnName("DeptName")
+                                                         .HasMaxLength(100).
+                                                         HasColumnType("varchar");
+        }
+    }
+}
---
+++ Demo/Contexts/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TPH
+            // modelBuilder.Entity<FullTimeEmployee>().HasBaseType<Employee>();
+            // modelBuilder.Entity<PartTimeEmployee>().HasBaseType<Employee>();
+
+            // TPC
+           // modelBuilder.Entity<Employee>().ToTable("Employee");
+           // modelBuilder.Entity<FullTimeEmployee>().ToTable("FullTimeEmployee");
+           // modelBuilder.Entity<PartTimeEmployee>().ToTable("PartTimeEmployee");
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-LA0JIU9\\MSSQLSERVER01; Database = App; Trusted_Connection = True; TrustServerCertificate = True");
+
+        }
+
+       // public DbSet<Employee> Employees { get; set; }
+        public DbSet<FullTimeEmployee> FullTimeEmployees { get; set; }
+        public DbSet<PartTimeEmployee> PartTimeEmployees { get; set; }
+    }
+}
